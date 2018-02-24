3fbb819c68dc5f009a3bad9b5348137a9c3cdaba
Ray Deck <ray@raydeck.com>
2018-02-24 10:26:24 -0500

    Fixing to work off the working path

--- bin/fixiosversion.js
+++ bin/fixiosversion.js
@@ -3,32 +3,21 @@ var pbxproj = require("xcode");
 var fs = require("fs");
 var path = require("path");
 var glob = require("glob");
-var targetVersion = "11.0";
-//Get my directory
-var thisPath = process.argv[1];
-var thisPath = path.dirname(thisPath); //bin directory
-var thisPath = path.dirname(thisPath); //dependency directory
-var thisPath = path.dirname(thisPath); // node_modules
-var baseName = path.basename(thisPath);
-const packagePath = baseName + "/package.json";
+var targetVersion = "11.2";
+const packagePath = path.join(process.cwd(), "package.json");
 if (fs.existsSync(packagePath)) {
   const package = require(packagePath);
   if (typeof package.iosTarget != undefined) {
     targetVersion = package.iosTarget;
   }
 }
-if (!baseName.startsWith("node_modules")) {
-  console.log("This is not a dependency: ", thisPath);
-  process.exit();
-}
-var thisPath = path.dirname(thisPath); // parent
-var iosPath = path.resolve(thisPath, "ios");
+var iosPath = path.resolve(process.cwd(), "ios");
 if (!fs.existsSync(iosPath)) {
   console.log("Could not find ios in ", thisPath, iosPath);
   console.log(fs.readdirSync(thisPath));
   process.exit();
 }
-xpdir = glob.sync(iosPath + "/*.xcodeproj")[0];
+xpdir = glob.sync(path.join(iosPath, "*.xcodeproj"))[0];
 if (xpdir.length === 0) {
   console.log("Could not find xcodeproj directory inside: ", iosPath);
   process.exit();
